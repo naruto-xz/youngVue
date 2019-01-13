180601c0a64a9b0bb7496e8a25bd28c2eb1e9eda
chaizhixiang@netposa.com <chaizhixiang@netposa.com>
2019-01-14 07:59:53 +0800

    路由

--- webapp/src/module/route/index.vue
+++ webapp/src/module/route/index.vue
@@ -2,7 +2,7 @@
   <!--<div>{{getId}}</div>-->
   <div>
     <div>{{title}}</div>
-    <button v-for="item in data" @click="change(item.value)">{{item.text}}</button>
+    <button v-for="item in data" @click="change(item)">{{item.text}}</button>
     <button @click="goBack">返回上一级页面</button>
     <router-view></router-view>
   </div>
@@ -13,21 +13,46 @@
       data(){
         return {
           title: "父路由",
+          bananaId: 3,
+          passId: 4,
           data: [
             {
+              name: "apple",
               text: "苹果",
               value: "/route/apple"
             },
             {
+              name: "orange",
               text: "橘子",
+              alias: "/route/o",
               value: "/route/orange"
+            },
+            {
+              name: "banana",
+              text: "香蕉",
+              value: "/route/banana"
+            },
+            {
+              name: "pass",
+              text: "路由组件传参",
+              value: "/route/pass"
             }
           ]
         }
       },
       methods: {
-        change(value){
-          this.$router.push(value)
+        change(item){
+          if(item.text == "香蕉"){
+            this.$router.push({path: `${item.value}/${this.bananaId}`})
+            // this.$router.push({name: item.name, params: {id : this.id}}) //命名路由
+          }else if(item.text == "橘子"){
+            this.$router.push(item.alias) //别名
+          }else if(item.text == "路由组件传参"){
+            this.$router.push({path: `${item.value}/${this.passId}`})
+          }else{
+            this.$router.push(item.value)
+            // this.$router.push({name: item.name})
+          }
         },
         goBack(){
          this.$router.go(-1);
---
+++ webapp/src/module/route/pass.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div>{{title}}</div>
+    <div>{{passId}}</div>
+  </div>
+</template>
+
+<script>
+    export default {
+      props: ["id"],
+      data(){
+       return {
+         title: "路由组件传参",
+         passId: "",
+       }
+      },
+      mounted() {
+        // this.passId =  this.$route.params.id; //该组件强依赖于定义的路由，不能被其它地方再复用;
+        this.passId =  this.id;   //靠路由传进来的id，不强依赖路由，可以在任何地方复用;
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
