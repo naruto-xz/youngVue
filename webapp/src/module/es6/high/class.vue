087e0b4fda23c26b4effca8ff1dbd0753c9f779c
chaizhixiang@netposa.com <chaizhixiang@netposa.com>
2019-02-22 08:01:03 +0800

    class

---
+++ webapp/src/module/es6/high/class.vue
@@ -0,0 +1,47 @@
+<template>
+
+</template>
+
+<script>
+    export default {
+      methods:{
+        operate(){
+
+          // function Point(x,y) {
+          //    this.x =x;
+          //    this.y =y;
+          // }
+          // Point.prototype.toString = function () {
+          //   return '('+this.x+','+this.y+')'
+          // };
+          // var p =new Point(1,2);
+          // console.log(p);
+
+          class Bar {
+            constructor(x,y){
+              this.x = x;
+              this.y = y;
+            }
+            // toString(){
+            //   return '('+this.x+','+this.y+')'
+            // }
+            toString(){
+              return '('+this.x+','+this.y+')'
+            }
+          }
+
+          var p = new Bar(1,2);
+          console.log(p);
+          console.log(p.toString());
+
+
+
+
+        }
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
